a8f6dc43cc757546612d9ceca28ed7c93802c0bb
gunterm <gunterm@70feae74-860a-0410-b6e4-f24f7a5e8c4f>
2006-02-21 22:58:25 +0000

    first attempts at the XML parser

--- src/net/sf/statcvs/input/SvnLogfileParser.java
+++ src/net/sf/statcvs/input/SvnLogfileParser.java
@@ -23,11 +23,16 @@
 
 package net.sf.statcvs.input;
 
+import java.io.File;
 import java.io.IOException;
-import java.io.Reader;
+import java.io.InputStream;
 import java.util.logging.Logger;
 
-import net.sf.statcvs.util.LookaheadReader;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.SAXException;
 
 /**
  * Parses a CVS logfile. A {@link Builder} must be specified which does
@@ -42,16 +47,16 @@ public class SvnLogfileParser {
 	private static Logger logger
 			= Logger.getLogger(SvnLogfileParser.class.getName());
 
-	private LookaheadReader logReader;
 	private SvnLogBuilder builder;
+	private InputStream logFile;
 
 	/**
 	 * Default Constructor
-	 * @param logReader a <tt>Reader</tt> containing the CVS logfile
+	 * @param logFile a <tt>Reader</tt> containing the CVS logfile
 	 * @param builder the builder that will process the log information
 	 */
-	public SvnLogfileParser(Reader logReader, SvnLogBuilder builder) {
-		this.logReader = new LookaheadReader(logReader);
+	public SvnLogfileParser(InputStream logFile, SvnLogBuilder builder) {
+		this.logFile = logFile;
 		this.builder = builder;
 	}
 	
@@ -64,51 +69,63 @@ public class SvnLogfileParser {
 	public void parse() throws LogSyntaxException, IOException {
         long startTime = System.currentTimeMillis();
 		logger.fine("starting to parse...");
-		eatNonCheckedInFileLines();
-		if (!this.logReader.hasNextLine()) {
-			throw new LogSyntaxException("Empty logfile!");
+		SAXParserFactory factory = SAXParserFactory.newInstance();
+		try {
+			SAXParser parser = factory.newSAXParser();
+			parser.parse(logFile, new SvnXmlLogFileHandler(builder));
+		} catch (ParserConfigurationException e) {
+			throw new LogSyntaxException(e.getMessage());
+		} catch (SAXException e) {
+			throw new LogSyntaxException(e.getMessage());
 		}
-		if (!"".equals(this.logReader.getCurrentLine())) {
-			throw new LogSyntaxException("Expected '?' or empty line at line "
-					+ this.logReader.getLineNumber() + ", but found '"
-					+ this.logReader.getCurrentLine() + "'");
-		}
-		eatEmptyLines();
-//		TODO: uncomment when tag/branch reports are added 
-//		boolean isLogWithoutSymbolicNames = false;
-		boolean isFirstFile = true;
-		do {
-			FileBlockParser parser = new FileBlockParser(
-					this.logReader, this.builder, isFirstFile);
-			parser.parse();
-			isFirstFile = false;
-//			if (parser.isLogWithoutSymbolicNames()) {
-//				isLogWithoutSymbolicNames = true;
-//			}
-			eatEmptyLines();
-		} while (this.logReader.hasNextLine());
+
+		
+		//		eatNonCheckedInFileLines();
+//		if (!this.logReader.hasNextLine()) {
+//			throw new LogSyntaxException("Empty logfile!");
+//		}
+//		if (!"".equals(this.logReader.getCurrentLine())) {
+//			throw new LogSyntaxException("Expected '?' or empty line at line "
+//					+ this.logReader.getLineNumber() + ", but found '"
+//					+ this.logReader.getCurrentLine() + "'");
+//		}
+//		eatEmptyLines();
+////		TODO: uncomment when tag/branch reports are added 
+////		boolean isLogWithoutSymbolicNames = false;
+//		boolean isFirstFile = true;
+//		do {
+//			FileBlockParser parser = new FileBlockParser(
+//					this.logReader, this.builder, isFirstFile);
+//			parser.parse();
+//			isFirstFile = false;
+////			if (parser.isLogWithoutSymbolicNames()) {
+////				isLogWithoutSymbolicNames = true;
+////			}
+//			eatEmptyLines();
+//		} while (this.logReader.hasNextLine());
 //		if (isLogWithoutSymbolicNames) {
 //			logger.warning("Log was created with '-N' switch of 'cvs log', some reports will be missing!");
 //		}
 		logger.fine("parsing finished in "
 				+ (System.currentTimeMillis() - startTime) + " ms.");
 	}
+	
 
-	private void eatNonCheckedInFileLines() throws IOException {
-		while (this.logReader.hasNextLine() &&
-				this.logReader.nextLine().startsWith("? ")) {
-			// ignore lines starting with "? "
-		}
-	}
-
-	/**
-	 * Calls nextLine() on the reader until EOF or a non-empty line
-	 * is found
-	 */
-	private void eatEmptyLines() throws IOException {
-		while (this.logReader.hasNextLine() &&
-				"".equals(this.logReader.nextLine())) {
-			// ignore empty lines
-		}
-	}
+//	private void eatNonCheckedInFileLines() throws IOException {
+//		while (this.logReader.hasNextLine() &&
+//				this.logReader.nextLine().startsWith("? ")) {
+//			// ignore lines starting with "? "
+//		}
+//	}
+//
+//	/**
+//	 * Calls nextLine() on the reader until EOF or a non-empty line
+//	 * is found
+//	 */
+//	private void eatEmptyLines() throws IOException {
+//		while (this.logReader.hasNextLine() &&
+//				"".equals(this.logReader.nextLine())) {
+//			// ignore empty lines
+//		}
+//	}
 }
---
+++ src/net/sf/statcvs/input/SvnXmlLogFileHandler.java
@@ -0,0 +1,172 @@
+package net.sf.statcvs.input;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.Date;
+import java.util.logging.Logger;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class SvnXmlLogFileHandler extends DefaultHandler {
+
+	private static final String AUTHOR = "author";
+	private static final String DATE = "date";
+	private static final String LOG = "log";
+	private static final String LOGENTRY = "logentry";
+	private static Logger logger = Logger.getLogger(SvnXmlLogFileHandler.class.getName());
+	private static final String MSG = "msg";
+	private static final String PATH = "path";
+	private static final String PATHS = "paths";
+	private SvnLogBuilder builder;
+	private RevisionData currentRevisionData;
+
+	private String lastElement = "";
+	private String currentElement = "";
+
+	public void characters(char[] ch, int start, int length) throws SAXException {
+		super.characters(ch, start, length);
+		String s = new String(ch, start, length);
+
+		if (currentElement.equals(AUTHOR)) {
+			currentRevisionData.setLoginName(s);
+		} else if (currentElement.equals(DATE)) {
+			
+	/*		Date dt;
+			try {
+				XmlCalendar.getInstance();
+
+				dt = DateFormat.getDateTimeInstance().parse(s);
+				currentRevisionData.setDate(dt);
+			} catch (ParseException e) {
+				warning("Invalid date specified");
+			}*/
+		}
+
+	}
+
+	public SvnXmlLogFileHandler(SvnLogBuilder builder) {
+		this.builder = builder;
+	}
+
+	public void endDocument() throws SAXException {
+		super.endDocument();
+	}
+
+	public void endElement(String uri, String localName, String qName) throws SAXException {
+		super.endElement(uri, localName, qName);
+		String eName = localName; // element name
+		if ("".equals(eName))
+			eName = qName; // namespaceAware = false
+
+		if (eName.equals(LOG)) {
+			if (!lastElement.equals(LOG)) {
+				fatalError("Invalid SVN log file.");
+			}
+			lastElement = "";
+		} else if (eName.equals(LOGENTRY)) {
+			if (!lastElement.equals(LOGENTRY)) {
+				fatalError("Invalid SVN log file.");
+			}
+			lastElement = LOG;
+		} else if (eName.equals(AUTHOR) || eName.equals(DATE) || eName.equals(MSG)) {
+			if (!lastElement.equals(LOGENTRY)) {
+				fatalError("Invalid SVN log file.");
+			}
+		} else if (eName.equals(PATHS)) {
+			if (!lastElement.equals(PATHS)) {
+				fatalError("Invalid SVN log file.");
+			}
+			lastElement = LOGENTRY;
+		} else if (eName.equals(PATH)) {
+			if (!lastElement.equals(PATHS)) {
+				fatalError("Invalid SVN log file.");
+			}
+		} else {
+			fatalError("Invalid SVN log file.");
+		}
+	}
+
+	public void error(SAXParseException e) throws SAXException {
+		// TODO Auto-generated method stub
+		super.error(e);
+	}
+
+	public void fatalError(SAXParseException e) throws SAXException {
+		// TODO Auto-generated method stub
+		super.fatalError(e);
+	}
+
+	private void fatalError(String message) throws SAXException {
+		fatalError(new SAXParseException(message, null));
+	}
+
+	public void startDocument() throws SAXException {
+		super.startDocument();
+	}
+
+	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+		// TODO Auto-generated method stub
+		super.startElement(uri, localName, qName, attributes);
+		String eName = localName; // element name
+		if ("".equals(eName))
+			eName = qName; // namespaceAware = false
+		// if (attrs != null) {
+		// for (int i = 0; i < attrs.getLength(); i++) {
+		// String aName = attrs.getLocalName(i); // Attr name
+		// if ("".equals(aName)) aName = attrs.getQName(i);
+		// }
+		// }
+
+		currentElement = eName;
+		if (eName.equals(LOG)) {
+			if (!lastElement.equals("")) {
+				fatalError("Invalid SVN log file.");
+			}
+			lastElement = LOG;
+
+			builder.buildModule("TODO: Path in repository of working folder");
+
+		} else if (eName.equals(LOGENTRY)) {
+			if (!lastElement.equals(LOG)) {
+				fatalError("Invalid SVN log file.");
+			}
+			lastElement = LOGENTRY;
+			currentRevisionData = new RevisionData();
+
+			if (attributes != null && attributes.getValue("revision") != null)
+				currentRevisionData.setRevisionNumber(attributes.getValue("revision"));
+			else
+				fatalError("Invalid SVN log file.");
+
+		} else if (eName.equals(AUTHOR) || eName.equals(DATE) || eName.equals(MSG)) {
+			if (!lastElement.equals(LOGENTRY)) {
+				fatalError("Invalid SVN log file.");
+			}
+
+		} else if (eName.equals(PATHS)) {
+			if (!lastElement.equals(LOGENTRY)) {
+				fatalError("Invalid SVN log file.");
+			}
+			lastElement = PATHS;
+		} else if (eName.equals(PATH)) {
+			if (!lastElement.equals(PATHS)) {
+				fatalError("Invalid SVN log file.");
+			}
+		} else {
+			fatalError("Invalid SVN log file.");
+		}
+	}
+
+	public void warning(SAXParseException e) throws SAXException {
+		// TODO Auto-generated method stub
+		super.warning(e);
+	}
+
+	private void warning(String message) throws SAXException {
+		warning(new SAXParseException(message, null));
+	}
+
+}
