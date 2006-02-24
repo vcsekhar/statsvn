626730d46d7a27609fd80f2b3bca4a0aa980a3a8
jkealey <jkealey@70feae74-860a-0410-b6e4-f24f7a5e8c4f>
2006-02-24 03:08:48 +0000

    can now know if a file (either deleted or not) is binary not 100% tested :)

--- src/net/sf/statcvs/input/SvnLogfileParser.java
+++ src/net/sf/statcvs/input/SvnLogfileParser.java
@@ -41,8 +41,7 @@ import net.sf.statcvs.util.SvnDiffUtils;
 import org.xml.sax.SAXException;
 
 /**
- * Parses a CVS logfile. A {@link Builder} must be specified which does the
- * construction work.
+ * Parses a CVS logfile. A {@link Builder} must be specified which does the construction work.
  * 
  * @author Anja Jentzsch
  * @author Richard Cyganiak
@@ -50,115 +49,123 @@ import org.xml.sax.SAXException;
  */
 public class SvnLogfileParser {
 
-	private static Logger logger = Logger.getLogger(SvnLogfileParser.class.getName());
+    private static Logger logger = Logger.getLogger(SvnLogfileParser.class.getName());
 
-	private SvnLogBuilder builder;
-	private InputStream logFile;
+    private SvnLogBuilder builder;
+    private InputStream logFile;
 
-	/**
-	 * Default Constructor
-	 * 
-	 * @param logFile
-	 *            a <tt>Reader</tt> containing the CVS logfile
-	 * @param builder
-	 *            the builder that will process the log information
-	 */
-	public SvnLogfileParser(InputStream logFile, SvnLogBuilder builder) {
-		this.logFile = logFile;
-		this.builder = builder;
-	}
+    /**
+     * Default Constructor
+     * 
+     * @param logFile
+     *            a <tt>Reader</tt> containing the CVS logfile
+     * @param builder
+     *            the builder that will process the log information
+     */
+    public SvnLogfileParser(InputStream logFile, SvnLogBuilder builder) {
+        this.logFile = logFile;
+        this.builder = builder;
+    }
 
-	/**
-	 * Parses the logfile. After <tt>parse()</tt> has finished, the result of
-	 * the parsing process can be obtained from the builder.
-	 * 
-	 * @throws LogSyntaxException
-	 *             if syntax errors in log
-	 * @throws IOException
-	 *             if errors while reading from the log Reader
-	 */
-	public void parse() throws LogSyntaxException, IOException {
-		long startTime = System.currentTimeMillis();
-		logger.fine("starting to parse...");
-		SAXParserFactory factory = SAXParserFactory.newInstance();
-		try {
-			SAXParser parser = factory.newSAXParser();
-			parser.parse(logFile, new SvnXmlLogFileHandler(builder));
-		} catch (ParserConfigurationException e) {
-			throw new LogSyntaxException(e.getMessage());
-		} catch (SAXException e) {
-			throw new LogSyntaxException(e.getMessage());
-		}
+    /**
+     * Parses the logfile. After <tt>parse()</tt> has finished, the result of the parsing process can be obtained from the builder.
+     * 
+     * @throws LogSyntaxException
+     *             if syntax errors in log
+     * @throws IOException
+     *             if errors while reading from the log Reader
+     */
+    public void parse() throws LogSyntaxException, IOException {
+        long startTime = System.currentTimeMillis();
+        logger.fine("starting to parse...");
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        try {
+            SAXParser parser = factory.newSAXParser();
+            parser.parse(logFile, new SvnXmlLogFileHandler(builder));
+        } catch (ParserConfigurationException e) {
+            throw new LogSyntaxException(e.getMessage());
+        } catch (SAXException e) {
+            throw new LogSyntaxException(e.getMessage());
+        }
 
-		// eatNonCheckedInFileLines();
-		// if (!this.logReader.hasNextLine()) {
-		// throw new LogSyntaxException("Empty logfile!");
-		// }
-		// if (!"".equals(this.logReader.getCurrentLine())) {
-		// throw new LogSyntaxException("Expected '?' or empty line at line "
-		// + this.logReader.getLineNumber() + ", but found '"
-		// + this.logReader.getCurrentLine() + "'");
-		// }
-		// eatEmptyLines();
-		// // TODO: uncomment when tag/branch reports are added
-		// // boolean isLogWithoutSymbolicNames = false;
-		// boolean isFirstFile = true;
-		// do {
-		// FileBlockParser parser = new FileBlockParser(
-		// this.logReader, this.builder, isFirstFile);
-		// parser.parse();
-		// isFirstFile = false;
-		// // if (parser.isLogWithoutSymbolicNames()) {
-		// // isLogWithoutSymbolicNames = true;
-		// // }
-		// eatEmptyLines();
-		// } while (this.logReader.hasNextLine());
-		// if (isLogWithoutSymbolicNames) {
-		// logger.warning("Log was created with '-N' switch of 'cvs log', some
-		// reports will be missing!");
-		// }
-		logger.fine("parsing svn log finished in " + (System.currentTimeMillis() - startTime) + " ms.");
+        // eatNonCheckedInFileLines();
+        // if (!this.logReader.hasNextLine()) {
+        // throw new LogSyntaxException("Empty logfile!");
+        // }
+        // if (!"".equals(this.logReader.getCurrentLine())) {
+        // throw new LogSyntaxException("Expected '?' or empty line at line "
+        // + this.logReader.getLineNumber() + ", but found '"
+        // + this.logReader.getCurrentLine() + "'");
+        // }
+        // eatEmptyLines();
+        // // TODO: uncomment when tag/branch reports are added
+        // // boolean isLogWithoutSymbolicNames = false;
+        // boolean isFirstFile = true;
+        // do {
+        // FileBlockParser parser = new FileBlockParser(
+        // this.logReader, this.builder, isFirstFile);
+        // parser.parse();
+        // isFirstFile = false;
+        // // if (parser.isLogWithoutSymbolicNames()) {
+        // // isLogWithoutSymbolicNames = true;
+        // // }
+        // eatEmptyLines();
+        // } while (this.logReader.hasNextLine());
+        // if (isLogWithoutSymbolicNames) {
+        // logger.warning("Log was created with '-N' switch of 'cvs log', some
+        // reports will be missing!");
+        // }
+        logger.fine("parsing svn log finished in " + (System.currentTimeMillis() - startTime) + " ms.");
 
-		Collection fileBuilders = builder.getFileBuilders().values();
-		for (Iterator iter = fileBuilders.iterator(); iter.hasNext();) {
-			FileBuilder fileBuilder = (FileBuilder) iter.next();
-			String fileName = fileBuilder.getName();
-			List revisions = fileBuilder.getRevisions();
-			for (int i = 0; i < revisions.size(); i++) {
-				if (i+1 < revisions.size()) {
-					String revNrNew = ((RevisionData) revisions.get(i)).getRevisionNumber();
-					String revNrOld = ((RevisionData) revisions.get(i+1)).getRevisionNumber();
-					System.out.println(fileName + " " + revNrOld + " " + revNrNew);
-					InputStream diffStream = SvnDiffUtils.callSvnDiff(revNrOld, revNrNew, fileName);
-					LookaheadReader diffReader = new LookaheadReader(new InputStreamReader(diffStream));
-					while (diffReader.hasNextLine()) {
-						diffReader.nextLine();
-		//				System.out.println(diffReader.getCurrentLine());
-					}
-					
-				}
-			}
-		}
+        Collection fileBuilders = builder.getFileBuilders().values();
+        for (Iterator iter = fileBuilders.iterator(); iter.hasNext();) {
+            FileBuilder fileBuilder = (FileBuilder) iter.next();
+            String fileName = fileBuilder.getName();
+            List revisions = fileBuilder.getRevisions();
+            for (int i = 0; i < revisions.size(); i++) {
+                if (i + 1 < revisions.size()) {
+                    String revNrNew = ((RevisionData) revisions.get(i)).getRevisionNumber();
+                    String revNrOld = ((RevisionData) revisions.get(i + 1)).getRevisionNumber();
+                    System.out.println(fileName + " " + revNrOld + " " + revNrNew);
+                    InputStream diffStream = SvnDiffUtils.callSvnDiff(revNrOld, revNrNew, fileName);
+                    LookaheadReader diffReader = new LookaheadReader(new InputStreamReader(diffStream));
+                    int added = -1;
+                    int removed = -1;
+                    while (diffReader.hasNextLine()) {
+                        diffReader.nextLine();
 
-		logger.fine("parsing svn diff finished in " + (System.currentTimeMillis() - startTime) + " ms.");
+                        // very simple algorithm
+                        if (diffReader.getCurrentLine().charAt(0) == '+')
+                            added++;
+                        else if (diffReader.getCurrentLine().charAt(0) == '-')
+                            removed++;
+                        // System.out.println(diffReader.getCurrentLine());
+                    }
+                    if (added!=-1 && removed!=-1)
+                        ((RevisionData) revisions.get(i + 1)).setLines(added, removed);
+                }
+            }
+        }
 
-	}
+        logger.fine("parsing svn diff finished in " + (System.currentTimeMillis() - startTime) + " ms.");
 
-	// private void eatNonCheckedInFileLines() throws IOException {
-	// while (this.logReader.hasNextLine() &&
-	// this.logReader.nextLine().startsWith("? ")) {
-	// // ignore lines starting with "? "
-	// }
-	// }
-	//
-	// /**
-	// * Calls nextLine() on the reader until EOF or a non-empty line
-	// * is found
-	// */
-	// private void eatEmptyLines() throws IOException {
-	// while (this.logReader.hasNextLine() &&
-	// "".equals(this.logReader.nextLine())) {
-	// // ignore empty lines
-	// }
-	// }
+    }
+
+    // private void eatNonCheckedInFileLines() throws IOException {
+    // while (this.logReader.hasNextLine() &&
+    // this.logReader.nextLine().startsWith("? ")) {
+    // // ignore lines starting with "? "
+    // }
+    // }
+    //
+    // /**
+    // * Calls nextLine() on the reader until EOF or a non-empty line
+    // * is found
+    // */
+    // private void eatEmptyLines() throws IOException {
+    // while (this.logReader.hasNextLine() &&
+    // "".equals(this.logReader.nextLine())) {
+    // // ignore empty lines
+    // }
+    // }
 }
