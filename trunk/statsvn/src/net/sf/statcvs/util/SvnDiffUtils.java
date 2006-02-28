3cac0e49a24c5835cfbde27f505374fb77a3dd22
jkealey <jkealey@70feae74-860a-0410-b6e4-f24f7a5e8c4f>
2006-02-28 13:58:38 +0000

    removed useless comments

--- trunk/statsvn/src/net/sf/statcvs/util/SvnDiffUtils.java
+++ trunk/statsvn/src/net/sf/statcvs/util/SvnDiffUtils.java
@@ -3,68 +3,37 @@ package net.sf.statcvs.util;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.InputStream;
-import java.io.OutputStream;
 
 import net.sf.statcvs.output.ConfigurationOptions;
 
 public class SvnDiffUtils {
 
-	public synchronized static InputStream callSvnDiff(String oldRevNr, String newRevNr) {
-		InputStream istream = null;
-		String svnDiffCommand = /*
-								 * FileUtils.getPathWithoutEndingSlash(ConfigurationOptions.getCheckedOutDirectory()) +
-								 * FileUtils.getDirSeparator() +
-								 */"svn diff -r " + oldRevNr + ":" + newRevNr + " --no-diff-deleted";
-		try {
-			Process p = Runtime.getRuntime().exec(svnDiffCommand, null,
-					new File(FileUtils.getPathWithoutEndingSlash(ConfigurationOptions.getCheckedOutDirectory()) + FileUtils.getDirSeparator()));
-			// OutputStream ostream = p.getOutputStream();
-			// ostream.write(input);
-			// ostream.close();
-			istream = new BufferedInputStream(p.getInputStream());
-		} catch (Exception e) {
-			e.printStackTrace();
-			/*
-			 * Status status = new Status(Status.ERROR, "seg.jUCMNav", 1,
-			 * e.toString(), e); //$NON-NLS-1$ ErrorDialog.openError(getShell(),
-			 * Messages.getString("AutoLayoutWizard.autoLayoutError"),
-			 * //$NON-NLS-1$
-			 * Messages.getString("AutoLayoutWizard.errorOccured"),
-			 * //$NON-NLS-1$ status, IStatus.ERROR | IStatus.WARNING);
-			 */
-			return null;
-		}
-		return istream;
-	}
+    public synchronized static InputStream callSvnDiff(String oldRevNr, String newRevNr) {
+        InputStream istream = null;
+        String svnDiffCommand = "svn diff -r " + oldRevNr + ":" + newRevNr + " --no-diff-deleted";
+        try {
+            Process p = Runtime.getRuntime().exec(svnDiffCommand, null,
+                    new File(FileUtils.getPathWithoutEndingSlash(ConfigurationOptions.getCheckedOutDirectory()) + FileUtils.getDirSeparator()));
+            istream = new BufferedInputStream(p.getInputStream());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        return istream;
+    }
 
-
-	public synchronized static InputStream callSvnDiff(String oldRevNr, String newRevNr, String filename) {
-		InputStream istream = null;
-		String svnDiffCommand = /*
-								 * FileUtils.getPathWithoutEndingSlash(ConfigurationOptions.getCheckedOutDirectory()) +
-								 * FileUtils.getDirSeparator() +
-								 */"svn diff -r " + oldRevNr + ":" + newRevNr + " --no-diff-deleted " + filename;
-		try {
-			Process p = Runtime.getRuntime().exec(svnDiffCommand, null,
-					new File(FileUtils.getPathWithoutEndingSlash(ConfigurationOptions.getCheckedOutDirectory()) + FileUtils.getDirSeparator()));
-			// OutputStream ostream = p.getOutputStream();
-			// ostream.write(input);
-			// ostream.close();
-			istream = new BufferedInputStream(p.getInputStream());
-		} catch (Exception e) {
-			e.printStackTrace();
-			/*
-			 * Status status = new Status(Status.ERROR, "seg.jUCMNav", 1,
-			 * e.toString(), e); //$NON-NLS-1$ ErrorDialog.openError(getShell(),
-			 * Messages.getString("AutoLayoutWizard.autoLayoutError"),
-			 * //$NON-NLS-1$
-			 * Messages.getString("AutoLayoutWizard.errorOccured"),
-			 * //$NON-NLS-1$ status, IStatus.ERROR | IStatus.WARNING);
-			 */
-			return null;
-		}
-		return istream;
-	}
-	
+    public synchronized static InputStream callSvnDiff(String oldRevNr, String newRevNr, String filename) {
+        InputStream istream = null;
+        String svnDiffCommand = "svn diff -r " + oldRevNr + ":" + newRevNr + " --no-diff-deleted " + filename;
+        try {
+            Process p = Runtime.getRuntime().exec(svnDiffCommand, null,
+                    new File(FileUtils.getPathWithoutEndingSlash(ConfigurationOptions.getCheckedOutDirectory()) + FileUtils.getDirSeparator()));
+            istream = new BufferedInputStream(p.getInputStream());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        return istream;
+    }
 
 }
