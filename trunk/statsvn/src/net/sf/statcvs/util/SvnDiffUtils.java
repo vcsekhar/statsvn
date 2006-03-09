75b42f5c379cb052f76a409bb2c88f1ec013bc5d
jkealey <jkealey@70feae74-860a-0410-b6e4-f24f7a5e8c4f>
2006-03-09 17:07:34 +0000

    now using same query for diff and now returns errorstream if it exists.

--- trunk/statsvn/src/net/sf/statcvs/util/SvnDiffUtils.java
+++ trunk/statsvn/src/net/sf/statcvs/util/SvnDiffUtils.java
@@ -4,8 +4,9 @@ import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 
-import net.sf.statcvs.input.RevisionData;
+import net.sf.statcvs.input.NoLineCountException;
 import net.sf.statcvs.output.ConfigurationOptions;
 
 public class SvnDiffUtils {
@@ -15,7 +16,10 @@ public class SvnDiffUtils {
 		String svnDiffCommand = "svn diff -r " + oldRevNr + ":" + newRevNr + " --no-diff-deleted";
 		try {
 			Process p = Runtime.getRuntime().exec(svnDiffCommand, null, ConfigurationOptions.getCheckedOutDirectoryAsFile());
-			istream = new BufferedInputStream(p.getInputStream());
+			if (p.getErrorStream().available() > 0) {
+				istream = new BufferedInputStream(p.getErrorStream());
+			} else
+				istream = new BufferedInputStream(p.getInputStream());
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
@@ -26,16 +30,21 @@ public class SvnDiffUtils {
 	private synchronized static InputStream callSvnDiff(String oldRevNr, String newRevNr, String filename, boolean deleted) {
 		InputStream istream = null;
 		String svnDiffCommand = null;
-		
+
 		filename = SvnInfoUtils.relativePathToUrl(filename);
-//		if (deleted) {
-			svnDiffCommand = "svn diff  --old " + filename + "@" + oldRevNr + "  --new " + filename + "@" + newRevNr;
-//		} else {
-//			svnDiffCommand = "svn diff -r " + oldRevNr + ":" + newRevNr + " --no-diff-deleted " + filename;
-//		}
+		// if (deleted) {
+		svnDiffCommand = "svn diff  --old " + filename + "@" + oldRevNr + "  --new " + filename + "@" + newRevNr;
+		// } else {
+		// svnDiffCommand = "svn diff -r " + oldRevNr + ":" + newRevNr + "
+		// --no-diff-deleted " + filename;
+		// }
 		try {
 			Process p = Runtime.getRuntime().exec(svnDiffCommand, null, ConfigurationOptions.getCheckedOutDirectoryAsFile());
-			istream = new BufferedInputStream(p.getInputStream());
+			if (p.getErrorStream().available() > 0) {
+				istream = new BufferedInputStream(p.getErrorStream());
+			} else
+				istream = new BufferedInputStream(p.getInputStream());
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
@@ -47,23 +56,17 @@ public class SvnDiffUtils {
 		InputStream diffStream = callSvnDiff(oldRevNr, newRevNr, filename, false);
 		LookaheadReader diffReader = new LookaheadReader(new InputStreamReader(diffStream));
 		int lineDiff[] = parseDiff(diffReader);
-		if (lineDiff[0] == -1 || lineDiff[1] == -1) {
-			// condition is true only for deleted files
-			String urlFileName = SvnInfoUtils.relativePathToUrl(filename);
-			diffStream = callSvnDiff(oldRevNr, newRevNr, urlFileName, true);
-			diffReader = new LookaheadReader(new InputStreamReader(diffStream));
-			lineDiff = parseDiff(diffReader);
-		}
 		return lineDiff;
 	}
 
 	private static int[] parseDiff(LookaheadReader diffReader) throws IOException {
 		int lineDiff[] = { -1, -1 };
 		if (!diffReader.hasNextLine()) {
-			// diff has no output because we modified properties or the changes are auto-generated ($id$ $author$ kind of thing)
+			// diff has no output because we modified properties or the changes
+			// are auto-generated ($id$ $author$ kind of thing)
 			// http://svnbook.red-bean.com/nightly/en/svn.advanced.props.html#svn.advanced.props.special.keywords
-			lineDiff[0]=0;
-			lineDiff[1]=0;
+			lineDiff[0] = 0;
+			lineDiff[1] = 0;
 		}
 		while (diffReader.hasNextLine()) {
 			diffReader.nextLine();
