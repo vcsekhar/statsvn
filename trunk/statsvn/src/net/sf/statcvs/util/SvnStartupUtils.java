c29e2d9912f5e749feb34d2106ddb9adff36ffd5
jpdaigle <jpdaigle@70feae74-860a-0410-b6e4-f24f7a5e8c4f>
2006-03-13 00:25:35 +0000

    Bug 16: check that "repository root" information is available from the "svn info" command. (A 1.3+ feature.) Fail on startup if not available.

--- trunk/statsvn/src/net/sf/statcvs/util/SvnStartupUtils.java
+++ trunk/statsvn/src/net/sf/statcvs/util/SvnStartupUtils.java
@@ -3,11 +3,12 @@ package net.sf.statcvs.util;
 import java.io.BufferedInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * Utilities class that verifies if the correct version of subversion is used.
+ * Utility class that verifies if the correct version of subversion is used.
  * 
  * @author Jean-Philippe Daigle <jpdaigle@softwareengineering.ca>
  * 
@@ -18,6 +19,9 @@ public class SvnStartupUtils {
     private static String SVN_MINIMUM_VERSION = "1.3.0";
     private static String SVN_VERSION_LINE_PATTERN = ".*version [0-9]+\\.[0-9]+\\.[0-9]+.*";
     private static String SVN_VERSION_PATTERN = "[0-9]+\\.[0-9]+\\.[0-9]+";
+    private static String SVN_INFO_WITHREPO_LINE_PATTERN= ".*<root>.+</root>.*";	//HACK: we "should" parse the output and check for a node named root, but this will work well enough
+    private static String SVN_REPO_ROOT_NOTFOUND = "Repository root not available - verify that the project was checked out with svn version " + SVN_MINIMUM_VERSION + " or above.";
+    private static Logger _logger = Logger.getLogger(SvnStartupUtils.class.getName());
 
     /**
      * Verifies that the current revision of SVN is SVN_MINIMUM_VERSION
@@ -35,7 +39,6 @@ public class SvnStartupUtils {
                 String line = reader.nextLine();
                 if (line.matches(SVN_VERSION_LINE_PATTERN)) {
                     // We have our version line
-                    // System.out.println(">>>" + line);
                     Pattern pRegex = Pattern.compile(SVN_VERSION_PATTERN);
                     Matcher m = pRegex.matcher(line);
                     if (m.find()) {
@@ -52,10 +55,41 @@ public class SvnStartupUtils {
             istream.close();
 
         } catch (Exception e) {
-            // should we send to logger instead?
-            e.printStackTrace();
+        		_logger.warning(e.getMessage());
         }
 
         throw new SvnVersionMismatchException();
     }
+    
+    /**
+     * Verifies that the "svn info" command can return the repository root
+     * (info available in svn >= 1.3.0)
+     * 
+     * @throws SvnVersionMismatchException
+     *             if <tt>svn info</tt> failed to provide a non-empty repository root
+     */
+    public synchronized static void checkRepoRootAvailable() throws SvnVersionMismatchException {
+        try {
+        		boolean ROOT_ONLY_TRUE = true;
+            InputStream istream = SvnInfoUtils.getSvnInfo(ROOT_ONLY_TRUE);
+            LookaheadReader reader = new LookaheadReader(new InputStreamReader(istream));
+
+            while (reader.hasNextLine()) {
+                String line = reader.nextLine();
+                if (line.matches(SVN_INFO_WITHREPO_LINE_PATTERN)) {
+                    // We have our <root> element in the svn info AND it's not empty --> checkout performed 
+                		// with a compatible version of subversion client.
+                		istream.close();
+                		return; // success
+                }
+            }
+            istream.close();
+
+        } catch (Exception e) {
+        		_logger.warning(e.getMessage());
+        }
+
+        throw new SvnVersionMismatchException(SVN_REPO_ROOT_NOTFOUND);
+
+	}
 }
