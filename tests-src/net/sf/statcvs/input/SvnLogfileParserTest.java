98d9bb0ba7072cb4ce79963aced10efcafd2ee24
jkealey <jkealey@70feae74-860a-0410-b6e4-f24f7a5e8c4f>
2006-04-04 20:21:26 +0000

    bug 12: some work towards re-integrating input layer level tests wrote a fixture that helps run statsvn without having to connect to the server, using files.

---
+++ tests-src/net/sf/statcvs/input/SvnLogfileParserTest.java
@@ -0,0 +1,30 @@
+package net.sf.statcvs.input;
+
+import java.io.IOException;
+
+import junit.framework.TestCase;
+import net.sf.statcvs.Main;
+import net.sf.statcvs.output.CommandLineParser;
+import net.sf.statcvs.output.ConfigurationException;
+import net.sf.statcvs.output.ConfigurationOptions;
+
+/**
+ * High-level scenarios to verify parsing without actually needing a server.
+ * 
+ * @author jkealey
+ * 
+ */
+public class SvnLogfileParserTest extends TestCase {
+
+	protected final static String sRoot = "./tests-src/net/sf/statcvs/input/samplefiles/";
+
+	protected RepositoryFileManager repFileMan;
+
+	public void testJUCMNav1() throws EmptyRepositoryException, ConfigurationException, IOException, LogSyntaxException {
+		String args[] = { "-title", "jUCMNav", "-output-dir", sRoot + "stats", sRoot + "seg.jUCMNav.log", sRoot };
+		new CommandLineParser(args).parse();
+		repFileMan = new DummyRepositoryFileManager(ConfigurationOptions.getCheckedOutDirectory(), sRoot + "seg.jUCMNav.info", sRoot + "seg.jUCMNav.propget",
+				sRoot + "seg.jUCMNav.linecounts");
+		Main.generateDefaultHTMLSuite(repFileMan);
+	}
+}
