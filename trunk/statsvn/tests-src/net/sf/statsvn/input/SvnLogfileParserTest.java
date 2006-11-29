d38dd46b7b09347357d8bfa87ad1cae60ee05de7
benoitx <benoitx@70feae74-860a-0410-b6e4-f24f7a5e8c4f>
2006-11-29 15:56:56 +0000

    [Bug 1602429] Patch for tests running in sandbox. Thanks to Miroslav Šulc - fordfrog

--- trunk/statsvn/tests-src/net/sf/statsvn/input/SvnLogfileParserTest.java
+++ trunk/statsvn/tests-src/net/sf/statsvn/input/SvnLogfileParserTest.java
@@ -23,7 +23,7 @@ public class SvnLogfileParserTest extends TestCase {
 	protected RepositoryFileManager repFileMan;
 
 	public void testJUCMNav1() throws EmptyRepositoryException, ConfigurationException, IOException, LogSyntaxException {
-		final String args[] = { "-title", "jUCMNav", "-output-dir", sRoot + "stats", sRoot + "seg.jUCMNav.log", sRoot };
+		final String args[] = { "-title", "jUCMNav", "-output-dir", sRoot + "stats", sRoot + "seg.jUCMNav.log", sRoot, "-cache-dir", sRoot  };
 		new SvnCommandLineParser(args).parse();
 		repFileMan = new DummyRepositoryFileManager(ConfigurationOptions.getCheckedOutDirectory(), sRoot + "seg.jUCMNav.info", sRoot + "seg.jUCMNav.propget",
 				sRoot + "seg.jUCMNav.linecounts");
