1e2b24e2eeb34f3315c21abbd0b0c933276b35e1
benoitx <benoitx@70feae74-860a-0410-b6e4-f24f7a5e8c4f>
2006-12-04 14:45:49 +0000

    [Bug 1608474]Removed 1.5-specific code. Thanks to samuli for spotting this and providing the solution.

--- src/net/sf/statsvn/util/SvnPropgetUtils.java
+++ src/net/sf/statsvn/util/SvnPropgetUtils.java
@@ -177,7 +177,7 @@ public final class SvnPropgetUtils {
 	 */
 	private static String getBinaryFilename(final String currentLine, final boolean removeRoot) {
 		// want to make sure we only have / in end result.
-		String line = removeRoot ? currentLine : currentLine.replace("\\", "/");
+		String line = removeRoot ? currentLine : currentLine.replaceAll("\\", "/");
 
 		// HACK: See bug 18. if removeRoot==true, no - will be found because we
 		// are calling for one specific file.
