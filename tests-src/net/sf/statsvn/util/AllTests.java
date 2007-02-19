e67f4f2be03d2de5b28ff063daaba815a7a2188a
benoitx <benoitx@70feae74-860a-0410-b6e4-f24f7a5e8c4f>
2007-02-19 09:40:46 +0000

    Added a unit test for SvnInfoUtil.replace(...)

--- tests-src/net/sf/statsvn/util/AllTests.java
+++ tests-src/net/sf/statsvn/util/AllTests.java
@@ -1,8 +1,8 @@
 /*
-    StatCvs - CVS statistics generation
+    StatCvs - CVS statistics generation 
     Copyright (C) 2002  Lukasz Pekacki <lukasz@pekacki.de>
     http://statcvs.sf.net/
-
+    
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Lesser General Public
     License as published by the Free Software Foundation; either
@@ -16,47 +16,37 @@
     You should have received a copy of the GNU Lesser General Public
     License along with this library; if not, write to the Free Software
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
-	$RCSfile: AllTests.java,v $
-	Created on $Date: 2002/08/23 14:49:29 $
+    
+	$RCSfile: AllTests.java,v $ 
+	Created on $Date: 2002/08/23 02:03:59 $ 
 */
-package net.sf.statsvn;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+package net.sf.statsvn.util;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
-import junit.textui.TestRunner;
 
 /**
- * JUnit Test suite containing all tests for StatCvs.
- *
- * @author Manuel Schulze
- * @version $Id: AllTests.java,v 1.13 2002/08/23 14:49:29 cyganiak Exp $
+ * JUnit tests for util classes.
+ * 
+ * @author Benoit Xhenseval
+ * @author Richard Cyganiak
+ * @author Jason Kealey
+ * @version $Id: AllTests.java,v 1.10 2002/08/23 02:03:59 cyganiak Exp $
  */
 public class AllTests {
 
 	/**
 	 * Method suite.
-	 * @return Test suite
+	 * @return Test Test suite
 	 */
 	public static Test suite() {
-		Logger.getLogger("net.sf.statcvs").setLevel(Level.OFF);
-		final TestSuite suite = new TestSuite("Test for net.sf.statcvs");
+		final TestSuite suite = new TestSuite("Test for net.sf.statsvn.util");
 		//$JUnit-BEGIN$
-		suite.addTest(net.sf.statsvn.input.AllTests.suite());
-		suite.addTest(net.sf.statsvn.util.AllTests.suite());
+
+		suite.addTestSuite(SvnInfoUtilTest.class);
+		
 		//$JUnit-END$
 		return suite;
 	}
-
-	/**
-	 * Runs all StatCvs unit tests with the text TestRunner
-	 * @param args Ignored.
-	 */
-	public static void main(final String[] args) {
-		TestRunner.run(suite());
-	}
 }
-
